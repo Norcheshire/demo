148ee6d06050256f081ad83a6772079531fa5249
Nor <tp6sji4@gmail.com>
2021-03-09 17:16:21 +0800

    123

---
+++ 天地創造設計部practice0304/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  天地創造設計部practice0304
+//
+//  Created by 陳諾 on 2021/3/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
